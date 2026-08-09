c8b6525af2691dda7b1d035120fbc4734e49ae20
Nidhi <nidhisingh5122005@gmail.com>
2026-08-09 11:42:21 +0530

    Time: 14 ms (5.49%) | Memory: 46 MB (67.34%) - LeetSync

--- 326-power-of-three/power-of-three.java
+++ 326-power-of-three/power-of-three.java
@@ -3,8 +3,7 @@ class Solution {
         return power(n, 0);
     }
     static boolean power(int n, int x){
-        if(n == 0)
-        return false;
+
 
         if(x > 19){
             return false;
