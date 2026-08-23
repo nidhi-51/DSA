3c42a37c97261d09c6576819f0f892c3597fe1e1
Nidhi <nidhisingh5122005@gmail.com>
2026-08-23 11:11:39 +0530

    Time: 21 ms (41.76%) | Memory: 45.3 MB (97.54%) - LeetSync

---
+++ 18-4sum/4sum.java
@@ -0,0 +1,48 @@
+class Solution {
+    public List<List<Integer>> fourSum(int[] nums, int target) {
+        List<List<Integer>> list = new ArrayList<>();
+        int n = nums.length;
+        // ye condition 4 element se jade ki liye na jaye uske liye kiya gya hai
+        if(n < 4){
+            return list;
+        }
+
+        Arrays.sort(nums);
+        for(int i = 0; i < n - 3; i++){
+            // to ignore duplicate value of i and j
+            if(i > 0 && nums[i] == nums[i-1]){
+                continue;
+            }
+            for(int j = i + 1; j < n - 2; j++){
+                if(j > i + 1 && nums[j] == nums[j-1]){
+                    continue;
+                }
+                int left = j + 1;
+                int right = n - 1;
+                while(left < right){
+                    long sum = (long) nums[i] + nums[j] + nums[left] + nums[right];
+                    if(sum == target){
+                        list.add(Arrays.asList(nums[i], nums[j], nums[left], nums[right]));
+                    
+                    // to ignore duplicate element of left and right
+                    while(left < right && nums[left] == nums[left + 1]){
+                        left++;
+                    }
+                    while(left < right && nums[right] == nums[right - 1]){
+                        right--;
+                    }
+                    left++;
+                    right--;
+                    }
+                    else if(sum < target){
+                        left++;
+                    }
+                    else{
+                        right--;
+                    }
+                }
+            }
+        }
+        return list;
+    }
+}
